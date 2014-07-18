0be36bd640377348721cc4938757f8e50ebcf7db
Christoffer Fink <christofferfink@gmail.com>
2014-07-18 17:12:04 +0200

    SingleAndMultiRangeNextTest: examine space.next
    Compares produced and expected values for various parameters.

--- main/src/net/finkn/inputspec/v050/Spec.java
+++ main/src/net/finkn/inputspec/v050/Spec.java
@@ -36,6 +36,7 @@ import org.junit.runners.Suite.SuiteClasses;
     BooleanLiteralsTest.class,
     IdLiteralsTest.class,
     MultiRangeMismatchTest.class,
+    SingleAndMultiRangeNextTest.class,
     SupportedParamIdsTest.class,
 })
 public class Spec {
