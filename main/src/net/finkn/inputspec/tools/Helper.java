826757e18b0ca93b282422f0422f88d638856bf5
Christoffer Fink <christofferfink@gmail.com>
2014-07-29 23:39:31 +0200

    Helper: add gen and sink test functions
    - Update a sink test case with a param builder.
    - Update a gen test case with a param builder.
    - Update a gen test case with multiple parameter configurations.

--- main/src/net/finkn/inputspec/tools/Helper.java
+++ main/src/net/finkn/inputspec/tools/Helper.java
@@ -22,12 +22,17 @@ package net.finkn.inputspec.tools;
 
 import net.finkn.inputspec.tools.types.Point;
 
+import se.miun.itm.input.model.InPUTException;
+
 /**
  * Helper for writing more concise tests.
  * Exports common configurations and small functions that are common to
  * multiple tests.
  */
 public class Helper {
+
+  private static final GenTestCase genTest = GenTestCase.getInstance();
+  private static final SinkTestCase sinkTest = SinkTestCase.getInstance();
 
   public static final ParamCfg pointParam = ParamCfg.builder()
       .id("X").inclMin("10").inclMax("20").add()
@@ -39,4 +44,21 @@ public class Helper {
   public static final MappingCfg pointMapping = MappingCfg.builder()
       .infer(pointParam, Point.class)
       .build();
+
+  public static ParamCfg.Builder pb() {
+    return ParamCfg.builder();
+  }
+
+  public static SinkTestCase sinkTest(ParamCfg.Builder builder) throws InPUTException {
+    return sinkTest.sink(Sink.fromParam(builder.build()));
+  }
+
+  public static GenTestCase genTest(ParamCfg.Builder builder) throws InPUTException {
+    return genTest.gen(Generator.fromParam(builder.build()));
+  }
+
+  public static GenTestCase genTest(String id, ParamCfg ... cfgs) throws InPUTException {
+    DesignSpaceCfg space = DesignSpaceCfg.builder().param(cfgs).build();
+    return genTest.gen(Generator.fromDesignSpace(space.getDesignSpace(), id));
+  }
 }
