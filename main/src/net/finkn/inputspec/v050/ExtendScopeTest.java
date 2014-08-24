e57f095c301138a1cbcce68cae4046f5637a816d
Christoffer Fink <christofferfink@gmail.com>
2014-08-24 20:52:30 +0200

    ExtendScopeTest: add more tests
    Added
    - circularExtensionsAreLegalButInvalid
    - serialExtensionIsValid
    - serialDelayedExtensionIsValid
    - parallelExtensionIsValid
    - intersectingDesignsAreLegal
    - extendingSelfIsLegal
    - originalParameterTakesPrecedence

--- main/src/net/finkn/inputspec/v050/ExtendScopeTest.java
+++ main/src/net/finkn/inputspec/v050/ExtendScopeTest.java
@@ -38,8 +38,10 @@ import se.miun.itm.input.model.design.IDesign;
  * @author Christoffer Fink
  */
 public class ExtendScopeTest {
+
   private final ParamCfg param1 = ParamCfg.builder().id("X1").build();
   private final ParamCfg param2 = ParamCfg.builder().id("X2").build();
+  private final ParamCfg param3 = ParamCfg.builder().id("X3").build();
   private final String id1 = param1.getId();
   private final String id2 = param2.getId();
   private IDesign design1;
@@ -52,18 +54,19 @@ public class ExtendScopeTest {
     // Make sure the wrong parameters are unsupported prior to extension.
     assertThat(design1.getValue(id2), is(nullValue()));
     assertThat(design2.getValue(id1), is(nullValue()));
-    design1.extendScope(design2);
   }
 
   /** A design can access the parameters of the extending design. */
   @Test
   public void canAccessParameterOfExtendingDesign() throws Throwable {
+    design1.extendScope(design2);
     assertThat(design1.getValue(id2), is(not(nullValue())));
   }
 
   /** Extending a design does not alter its set of supported IDs. */
   @Test
   public void extendScopeDoesNotUpdateSupportedIds() throws Throwable {
+    design1.extendScope(design2);
     // TODO: Prime candidate for a custom matcher.
     assertThat(design1.getSupportedParamIds(), not(hasItem(id2)));
   }
@@ -71,10 +74,94 @@ public class ExtendScopeTest {
   /** Changes to a parameter are reflected in both designs. */
   @Test
   public void settingValueUpdatesBothDesigns() throws Throwable {
+    design1.extendScope(design2);
     int old = design2.getValue(id2);
     int value = old + 1;
     design1.setValue(id2, value);
     assertThat(design1.getValue(id2), is(equalTo(value)));
     assertThat(design2.getValue(id2), is(equalTo(value)));
+  }
+
+  /**
+   * Extending A by B and then B by A is legal, but getValue no longer works
+   * as expected. Rather than returning {@code null} when given an invalid
+   * ID, the call results in a StackOverflowError instead.
+   */
+  @Test(expected = StackOverflowError.class)
+  public void circularExtensionsAreLegalButInvalid() throws Throwable {
+    design1.extendScope(design2);
+    design2.extendScope(design1);
+    design1.getValue("BANKAI"); // Should normally be null.
+  }
+
+  /**
+   * Extending B by C and A by B has the expected effect.
+   * All the parameters from the three designs can be accessed through A.
+   */
+  @Test
+  public void serialExtensionIsValid() throws Throwable {
+    IDesign design3 = design(param3);
+    design2.extendScope(design3);
+    design1.extendScope(design2);
+    assertThat(design1.getValue(param3.getId()), is(not(nullValue())));
+  }
+
+  /**
+   * Extending A by B and then B by C, that is, retroactively extending the
+   * extending design, has the same effect as extending B by C first, and then
+   * A by B, as shown in {@link #serialExtensionIsValid}.
+   */
+  @Test
+  public void serialDelayedExtensionIsValid() throws Throwable {
+    IDesign design3 = design(param3);
+    design1.extendScope(design2);
+    design2.extendScope(design3);
+    assertThat(design1.getValue(param3.getId()), is(not(nullValue())));
+  }
+
+  /**
+   * Extending A by both B and C has the expected effect.
+   * All the parameters from the three designs can be accessed through A.
+   */
+  @Test
+  public void parallelExtensionIsValid() throws Throwable {
+    IDesign design3 = design(param3);
+    design1.extendScope(design2);
+    design1.extendScope(design3);
+    assertThat(design1.getValue(param2.getId()), is(not(nullValue())));
+    assertThat(design1.getValue(param3.getId()), is(not(nullValue())));
+  }
+
+  /**
+   * Extending a design with a design such that one or more parameter IDs
+   * occur in both designs is legal.
+   */
+  @Test
+  public void intersectingDesignsAreLegal() throws Throwable {
+    design(param1).extendScope(design(param1));
+  }
+
+  /** Extending a design with itself is legal. */
+  @Test
+  public void extendingSelfIsLegal() throws Throwable {
+    design1.extendScope(design1);
+  }
+
+  /**
+   * When a parameter ID occurs in the intersection of two designs, then
+   * the parameter in the extending design is ignored.
+   */
+  @Test
+  public void originalParameterTakesPrecedence() throws Throwable {
+    ParamCfg x1 = ParamCfg.builder().fixed(1).build();
+    ParamCfg x2 = ParamCfg.builder().fixed(2).build();
+
+    test(design(x1), design(x2), 1);
+    test(design(x2), design(x1), 2);
+  }
+
+  private void test(IDesign d1, IDesign d2, int val) throws Throwable {
+    d1.extendScope(d2);
+    assertThat(d1.getValue("X"), is(equalTo(val)));
   }
 }
